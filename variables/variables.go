4d2957cf4d9c3cb957cf4071541df68efe747ab1
Mihir Chakma <mihirchakmaths@gmail.com>
2023-09-29 11:45:39 +0530

    add go.work file

---
+++ variables/variables.go
@@ -0,0 +1,38 @@
+package main
+
+import ("fmt")
+
+func main() {
+	
+	var myName = "Mihir Chakma"
+	fmt.Println("My name is", myName)
+
+	var name string = "Lisa"
+	fmt.Println("Name =", name)
+
+	userName := "Admin"
+	fmt.Println("Username =", userName)
+
+	var sum int
+	fmt.Println("The sum is =", sum)
+
+	part1, other := 1, 5
+	fmt.Println("Part 1 =", part1, "other =", other)
+
+	part2, other := 2, 0
+	fmt.Println("Part 2 =", part2, "other =", other)
+
+	sum = part1 + part2
+	fmt.Println("Sum is =", sum)
+
+	var (
+		lessonName = "Variables"
+		lessonType = "Demo"
+	)
+	fmt.Println("Lesson Name =", lessonName)
+	fmt.Println("Lesson Type =", lessonType)
+
+	word1, word2, _ := "Hello", "World", "!"
+	fmt.Println(word1, word2)
+
+}
