23264e9b7e2745038c330ffeb7b3f95a869d5d47
Mihir Chakma <mihirchakmaths@gmail.com>
2023-09-30 17:03:14 +0530

    updates

---
+++ datatypes/datatypes.go
@@ -0,0 +1,17 @@
+package main
+
+import ("fmt")
+
+func main() {
+	
+	var grade int = 10 // Integer
+	var gpa float32 = 4.0 // Floating Point Number
+	var isRaining bool = true // Boolean
+	var greet string = "Hi!" // String
+
+	fmt.Println(grade)
+	fmt.Println(gpa)
+	fmt.Println(isRaining)
+	fmt.Println(greet)
+
+}
---
+++ datatypes/go.mod
@@ -0,0 +1,3 @@
+module example.com/datatypes
+
+go 1.21.1
