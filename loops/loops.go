17dafd829e14b5259e2363d76f965ac47e78f1b3
Mihir Chakma <mihirchakmaths@gmail.com>
2023-10-05 17:57:23 +0530

    updates & add loop exercise

--- loops/loops.go
+++ loops/loops.go
@@ -4,25 +4,47 @@ import "fmt"
 
 func main() {
 
+
+    // for loop
+    for j := 7; j <= 9; j++ {
+        fmt.Println(j)
+    }
+    
+    // while loop
     i := 1
     for i <= 3 {
         fmt.Println(i)
         i = i + 1
     }
 
-    for j := 7; j <= 9; j++ {
-        fmt.Println(j)
-    }
-
+    // break
     for {
         fmt.Println("loop")
         break
     }
 
+    // continue
     for n := 0; n <= 5; n++ {
         if n%2 == 0 {
             continue
         }
         fmt.Println(n)
+    }
+
+    // Basic for loop
+    sum := 0
+    fmt.Println("Sum is", sum)
+
+    for i := 1; i <= 10; i++ {
+        sum += i // sum = sum + 1
+        fmt.Println("Increment. Sum is", sum)
+    }
+
+    fmt.Println("----------------------")
+
+    // while loop in go
+    for sum > 10 {
+        sum -= 5
+        fmt.Println("Decrement. Sum is", sum)
     }
 }
