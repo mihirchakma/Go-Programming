8eb5ffc338c219f59c6555e04a1323d9bddbefbf
Mihir Chakma <mihirchakmaths@gmail.com>
2023-10-10 15:39:28 +0530

    update example struct

--- examples/struct/struct.go
+++ examples/struct/struct.go
@@ -9,6 +9,11 @@ type Car struct {
 	width int
 	FrontWheel Wheel
 	BackWheel Wheel
+
+	/*Wheel struct { // anonymous structure
+		Radius int
+		Material string
+	}*/
 }
 
 type Wheel struct {
@@ -16,9 +21,26 @@ type Wheel struct {
 	Material string
 }
 
+// Anonymous Structs
+/*
+myCar := struct {
+	Make string
+	Model string
+} {
+	Make: "Tesla"
+	Model: "Model 3"
+}
+*/
+
 func main() {
 
 	toyota := Car{}
+	toyota.Make = "Japan"
+	toyota.Model = "Corolla"
+	toyota.height = 50
+	toyota.width = 40
 	toyota.FrontWheel.Radius = 5
+	fmt.Println(toyota.Make)
 	fmt.Println(toyota.FrontWheel.Radius)
+
 }
