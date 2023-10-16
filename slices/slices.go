e4e68daa272cc0c6cf2a59fc17cb6cac8fcf2c66
Mihir Chakma <mihirchakmaths@gmail.com>
2023-10-16 19:53:54 +0530

    slices updated

--- slices/slices.go
+++ slices/slices.go
@@ -115,4 +115,27 @@ func main() {
 	fmt.Println(slice5)
 	// fmt.Println(slice6)
 
+	
+	//* ZTM 
+	route := []string {"Grocery", "Department Store", "Salon"}
+	printSlice("Route 1", route)
+
+	route = append(route, "Home")
+	printSlice("Route 2", route)
+
+	fmt.Println()
+	fmt.Println(route[0], "Visited")
+	fmt.Println(route[1], "Visited")
+	
+	route = route[2:]
+	printSlice("Remaining locations", route)
+}
+
+func printSlice(title string, slice []string) {
+	fmt.Println()
+	fmt.Println("---", title, "---")
+	for i := 0; i < len(slice); i++ {
+		element := slice[i]
+		fmt.Println(element)
+	}
 }
